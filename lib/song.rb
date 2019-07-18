e5737c5dc3daf80d3432c75a9053323697441383
David Foote <david.a.foote@gmail.com>
2019-07-18 23:06:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
-    song = Song.new(song_name)
+    song = Song.new(song)
     artist.object = Artist.find_or_create_by_filename(file_name.split(" - ")[0])
     song.artist = artist.object
     song.artist = add_song(self)
