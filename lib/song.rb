e5faaa82b0f8067d43ef587d6d22f44871ea7ba2
David Foote <david.a.foote@gmail.com>
2019-07-18 23:22:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   
       artist, title = file_name.split(" - ")
       self.new(title).tap {|song|
-      song.artist = Artist.find_or_create_by_name(artist)
+      #song.artist = Artist.find_or_create_by_name(artist)
       song.artist.add_song(song)
       }
    end
