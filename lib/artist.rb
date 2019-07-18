42c7ad13bebcff5694bd93b58b532ccd96e4645e
David Foote <david.a.foote@gmail.com>
2019-07-18 23:18:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,31 +31,31 @@ class Artist
     self.class.all << self
   end
   
-  def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
-  end
-  
-  def self.find(name)
-    @@all.find do |artist|
-      artist.name = name
-    end
-  end
-  
-  def self.create(name)
-    artist = self.new(name)
-    artist.save
-    artist
-  end
-  
   # def self.find_or_create_by_name(name)
-  #    (my_artist = self.find.all {|artist| artist.name == name}) ? my_artist : self.new(name)
-    
-  #   self.find.all {|artist| artist.name == name} || self.new(name)
+  #   if self.find(name)
+  #     self.find(name)
+  #   else
+  #     self.create(name)
+  #   end
   # end
+  
+  # def self.find(name)
+  #   @@all.find do |artist|
+  #     artist.name = name
+  #   end
+  # end
+  
+  # def self.create(name)
+  #   artist = self.new(name)
+  #   artist.save
+  #   artist
+  # end
+  
+  def self.find_or_create_by_name(name)
+      (my_artist = self.find.all {|artist| artist.name == name}) ? my_artist : self.new(name)
+    
+    self.find.all {|artist| artist.name == name} || self.new(name)
+  end
   
   def print_songs
    #@songs.collect {|song| puts song.name}
