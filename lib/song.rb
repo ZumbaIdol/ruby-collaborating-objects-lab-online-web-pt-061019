d7b9b88c47405d522a5c9c2518cef7850a869e82
David Foote <david.a.foote@gmail.com>
2019-07-18 23:14:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   #   new_instance.artist.add_song(new_instance)
   #   new_instance
   
-      artist, title = file_name.split(" -")
+      artist, title = file_name.split(" - ")
       self.new(title).tap {|song|
       song.artist = Artist.find_or_create_by_name(artist)
       song.artist.add_song(song)
